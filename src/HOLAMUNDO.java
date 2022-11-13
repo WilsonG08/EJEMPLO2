1461ae826dd541d115d908edf3365febfe27eec0
WilsonG08 <wilson.guayanay@epn.edu.ec>
2022-11-12 19:22:03 -0500

    HICE UNA SUMA DE 2 VARIABLES

--- src/HOLAMUNDO.java
+++ src/HOLAMUNDO.java
@@ -1,5 +1,10 @@
 public class HOLAMUNDO {
     public static void main(String[] args) {
         System.out.println("HOLa BB");
+
+        int n1=0,n2=0, s=0;
+        s=n1+n2;
+
+        System.out.println("La suma es : "+s);
     }
 }
