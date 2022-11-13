32b7a3138cb648bf3b36e84c6bd443d8f10db6bb
KevinVeliz <mario-veliz258934@hotmail.com>
2022-11-12 19:27:32 -0500

    Hice una resta

--- src/HOLAMUNDO.java
+++ src/HOLAMUNDO.java
@@ -6,5 +6,10 @@ public class HOLAMUNDO {
         s=n1+n2;
 
         System.out.println("La suma es : "+s);
+        
+        int n11=0,n22=0,r=0;
+        r = n11-n22;
+        System.out.println("La resta es : "+r);
+        
     }
 }
